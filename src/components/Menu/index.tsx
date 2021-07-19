293d87c0656f34356018cb6a16ba4e599ab5617b
SanderNagtegaal <nagtegaal.sander@gmail.com>
2021-07-19 13:38:17 +0200

    Made the menu wider so that the items fit now

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -57,7 +57,7 @@ const StyledMenu = styled.div`
 `
 
 const MenuFlyout = styled.span<{ flyoutAlignment?: FlyoutAlignment }>`
-  min-width: 8.125rem;
+  min-width: 11rem;
   background-color: ${({ theme }) => theme.bg2};
   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
     0px 24px 32px rgba(0, 0, 0, 0.01);
